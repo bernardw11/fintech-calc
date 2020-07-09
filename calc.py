89d1c8ca86e5561bf0c53724c22c6880abc5976d
Bernardw11 <berniewang11@gmail.com>
2020-07-09 14:14:24 -0400

    mvp done

--- calc.py
+++ calc.py
@@ -6,12 +6,33 @@ board = [
 ]
 
 def run(board):
-    while not game_over(board):
+    turn = "x"
+    while not game_results(board):
+        print_board(board)
+        print(f"IT IS PLAYER {turn}'s TURN!!!!!!!!")
+        
+        i = int(input("Choose a row number: "))
+        j = int(input("Choose a column number: "))
+        while not is_legal(i, j, board):
+            print("THAT IS NOT A LEGAL MOVE FOOL")
+            i = int(input("Choose a row number: "))
+            j = int(input("Choose a column number: "))
+        board[i - 1][j - 1] = turn
 
+        if turn == "x":
+            turn = "o"
+        else:
+            turn = "x"
+    if game_results(board) == "tie":
+        print("TIE GAME")
+    else:
+        print(f"{game_results(board)} wins!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print_board(board)
 
-    print(f"{game_over(board)} wins!!!!!!!!!!!!!!!!!!!!!!!!!!")
+def is_legal(i, j, board):
+    return i in range(1, 4) and j in range(1, 4) and board[i - 1][j - 1] == " "
 
-def game_over(board):
+def game_results(board):
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2] and (board[i][0] != " "):
             return board[i][0]
@@ -21,12 +42,18 @@ def game_over(board):
         return board[0][0]
     if board[0][2] == board[1][1] == board[2][0] and (board[0][2] != " "):
         return board[0][2]
-    return ""
+    for row in board:
+        for space in row:
+            if space == " ":
+                return ""
+    return "tie"
 
 def print_board(board):
+    print(" 1 2 3")
     line = "|"
-    output = [str(3 - (i)) + line.join(board[i]) for i in range(3)]
+    output = [str(i + 1) + line.join(board[i]) for i in range(3)]
     linebreak = "\n -----\n"
     print(linebreak.join(output))
-    print(" 1 2 3")
-print_board(board)
+
+
+run(board)
